1ddfd6cf9c7e3f3d743a7b479dba52ff5358b966
Alexander Shabunevich <alexander.shabunevich@gmail.com>
2018-06-12 10:19:26 +0300

    Testing compatibility with .NET Core 2.1

---
+++ AspNetCore.CacheOutput.Demo.NetCore21/Startup.cs
@@ -0,0 +1,48 @@
+﻿using AspNetCore.CacheOutput.Extensions;
+using AspNetCore.CacheOutput.InMemory;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace AspNetCore.CacheOutput.Demo.NetCore21
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddSingleton<ICacheKeyGenerator, DefaultCacheKeyGenerator>();
+            services.AddSingleton<IApiOutputCache, InMemoryOutputCacheProvider>();
+
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            // Put app.UseCacheOutput() before any other middleware (even app.UseDeveloperExceptionPage())
+            app.UseCacheOutput();
+
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseMvc();
+        }
+    }
+}
