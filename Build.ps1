a72302c44c9422725d93b21f77a494621a05b25b
Mike Goodfellow <mgoodfellow@users.noreply.github.com>
2021-05-05 07:54:07 +0100

    Update for net5 and update to stackexchange redis client (#14)

---
+++ Build.ps1
@@ -0,0 +1,39 @@
+# Taken from https://github.com/jbogard/MediatR
+# Taken from psake https://github.com/psake/psake
+
+<#
+.SYNOPSIS
+  This is a helper function that runs a scriptblock and checks the PS variable $lastexitcode
+  to see if an error occcured. If an error is detected then an exception is thrown.
+  This function allows you to run command-line programs without having to
+  explicitly check the $lastexitcode variable.
+.EXAMPLE
+  exec { svn info $repository_trunk } "Error executing SVN. Please verify SVN command-line client is installed"
+#>
+function Exec {
+    [CmdletBinding()]
+    param(
+        [Parameter(Position = 0, Mandatory = 1)][scriptblock]$cmd,
+        [Parameter(Position = 1, Mandatory = 0)][string]$errorMessage = ($msgs.error_bad_command -f $cmd)
+    )
+    & $cmd
+    if ($lastexitcode -ne 0) {
+        throw ("Exec: " + $errorMessage)
+    }
+}
+
+$artifacts = ".\artifacts"
+
+if (Test-Path $artifacts) { Remove-Item $artifacts -Force -Recurse }
+
+exec { & dotnet clean -c Release }
+
+exec { & dotnet build -c Release }
+
+exec { & dotnet test -c Release -r $artifacts --no-build -l trx --verbosity=normal }
+
+exec { & dotnet pack .\AspNetCore.CacheOutput\AspNetCore.CacheOutput.csproj -c Release -o $artifacts --no-build }
+
+exec { & dotnet pack .\AspNetCore.CacheOutput.InMemory\AspNetCore.CacheOutput.InMemory.csproj -c Release -o $artifacts --no-build }
+
+exec { & dotnet pack .\AspNetCore.CacheOutput.Redis\AspNetCore.CacheOutput.Redis.csproj -c Release -o $artifacts --no-build }
