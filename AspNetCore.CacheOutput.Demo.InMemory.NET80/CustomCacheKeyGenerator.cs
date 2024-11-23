f4bbf9cc3437f20ece47403872eefe2be1936dac
Alexander Shabunevich <alexander.shabunevich@gmail.com>
2024-11-23 21:17:57 +0000

    Enable http port to allow testing Etag. Change HTTP port number, because 5000 port is being used in Mac OS.

---
+++ AspNetCore.CacheOutput.Demo.InMemory.NET80/CustomCacheKeyGenerator.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace AspNetCore.CacheOutput.Demo.InMemory.NET80
+{
+    public class CustomCacheKeyGenerator : DefaultCacheKeyGenerator
+    {
+        public override string MakeCacheKey(
+            ActionExecutingContext context,
+            string mediaType,
+            bool excludeQueryString = false
+        )
+        {
+            return base.MakeCacheKey(context, mediaType, excludeQueryString);
+        }
+    }
+}
