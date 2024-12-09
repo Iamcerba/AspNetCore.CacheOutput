9065e5e956a1acd685b2e4d10882b4d052773c9b
Alexander Shabunevich <alexander.shabunevich@gmail.com>
2024-12-09 18:18:01 +0300

    Cleanup. Disable optimization for debug configuration explicitly.

--- AspNetCore.CacheOutput.Demo.InMemory/Controllers/WeatherForecastController.cs
+++ AspNetCore.CacheOutput.Demo.InMemory/Controllers/WeatherForecastController.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using AspNetCore.CacheOutput.Demo.InMemory.Exceptions;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Logging;
 
 namespace AspNetCore.CacheOutput.Demo.InMemory.Controllers
 {
@@ -15,13 +14,6 @@ namespace AspNetCore.CacheOutput.Demo.InMemory.Controllers
         {
             "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
         };
-
-        private readonly ILogger<WeatherForecastController> _logger;
-
-        public WeatherForecastController(ILogger<WeatherForecastController> logger)
-        {
-            _logger = logger;
-        }
 
         [HttpGet]
         [CacheOutput(
